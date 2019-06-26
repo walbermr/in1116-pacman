467b9fc34a7e07cdedce2f15742135ee4fcfcd95
walbermr <wmr@cin.ufpe.br>
2019-06-25 22:12:08 -0300

    Added ghost implementation

---
+++ bfs.py
@@ -0,0 +1,69 @@
+from collections import deque
+
+def testLegal(maze, p):
+  if isinstance(maze, list):
+    height = len(maze)
+    width = len(maze[0])
+  else:
+    height = maze.height
+    width = maze.width
+
+  if p[0] < 0 or p[0] > width:
+    return False
+  if p[1] < 0 or p[1] > height:
+    return False
+  if maze[p[0]][p[1]] == True:
+    return False
+
+  return True    
+
+def make_graph(maze):
+  graph = []
+  directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+  height = maze.height
+  width = maze.width
+
+  for x in range(width):
+    for y in range(height):
+      p = (x, y)
+      node = {'point':p, 'neighbors':[]}
+      if testLegal(maze, p):
+        for d in directions:
+          neighbor = (p[0] + d[0], p[1] + d[1])
+          if testLegal(maze, neighbor):
+            node['neighbors'].append(neighbor)
+      graph.append(node)
+
+  return graph
+
+def bfs(start, goal, graph):
+  """
+  finds a shortest path in undirected `graph` between `start` and `goal`. 
+  If no path is found, returns `None`
+  """
+  if start == goal:
+      return [start]
+  visited = {start}
+  queue = deque([(start, [])])
+
+  while queue:
+    current, path = queue.popleft()
+    visited.add(current)
+    
+    neighbors = None
+    for node in graph:
+      if node['point'] == current:
+        neighbors = node['neighbors']
+
+    for neighbor in neighbors:
+      if neighbor == goal:
+        return path + [current, neighbor]
+      if neighbor in visited:
+        continue
+      queue.append((neighbor, path + [current]))
+      visited.add(neighbor)   
+  return None  # no path found. not strictly needed        
+
+def search(begin, end, maze):
+  graph = make_graph(maze)
+  return bfs(begin, end, graph)
--- ghostAgents.py
+++ ghostAgents.py
@@ -12,10 +12,13 @@ from game import Directions
 import random
 from util import manhattanDistance
 import util
+import bfs
 
 class GhostAgent( Agent ):
   def __init__( self, index ):
     self.index = index
+    self.prob_scared_flee = 0.99
+    self.prob_attack = 0.99
 
   def getAction( self, state ):
     dist = self.getDistribution(state)
@@ -29,10 +32,84 @@ class GhostAgent( Agent ):
     util.raiseNotDefined()
 
 class RuleGhost( GhostAgent ):
-  def getAction(self, state):
-    "*** OUR CODE HERE MAGO ***"
-    util.raiseNotDefined()
 
+  def __chokeSearch(self, state, visited, queue):
+    directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+    choke_points = []
+
+    while len(queue) > 0:
+      p = queue.pop(0)
+      possibilities = []
+      
+      for d in directions:
+        new_point = (p[0] + d[0], p[1] + d[1])
+        is_legal = bfs.testLegal(state.getWalls(), new_point)
+
+        if is_legal and new_point not in visited:
+          possibilities.append(new_point)
+          
+      if len(possibilities) > 1:
+        choke_points.append(visited[len(visited) - 1])
+      elif len(possibilities) > 0:
+        queue.append(possibilities[0])
+
+      visited.append(p)
+    
+    return choke_points
+
+  def getChockingPoints(self, state):
+    starting_point = state.getPacmanPosition()
+    possible_sorroundings = []
+    directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+
+    for d in directions:
+      x = starting_point[0] + d[0]
+      y = starting_point[1] + d[1]
+      if bfs.testLegal(state.getWalls(), (x,y)):
+        possible_sorroundings.append((x,y))
+    
+    # print 'choke - ' self.__chokeSearch(state, [starting_point], possible_sorroundings))
+    return self.__chokeSearch(state, [starting_point], possible_sorroundings)
+
+  def getDistribution( self, state ):
+    # Read variables from state
+    ghostState = state.getGhostState( self.index )
+    legalActions = state.getLegalActions( self.index )
+    pos = state.getGhostPosition( self.index )
+    isScared = ghostState.scaredTimer > 0
+    choke_points = self.getChockingPoints(state)
+    target_point = None
+
+    if len(choke_points) > 1:
+      target_point = choke_points[self.index - 1]
+    else:
+      target_point = state.getPacmanPosition()
+    
+    speed = 1
+    if isScared: speed = 0.5
+    
+    actionVectors = [Actions.directionToVector( a, speed ) for a in legalActions]
+    newPositions = [( int(pos[0]+a[0]), int(pos[1]+a[1]) ) for a in actionVectors]
+
+    # Select best actions given the state
+
+    distancesToTarget = [ len(bfs.search(pos, target_point, state.getWalls())) for pos in newPositions]
+
+    if isScared:
+      bestScore = max( distancesToTarget )
+      bestProb = self.prob_scared_flee
+    else:
+      bestScore = min( distancesToTarget )
+      bestProb = self.prob_attack
+    bestActions = [action for action, distance in zip( legalActions, distancesToTarget ) if distance == bestScore]
+    
+    # Construct distribution
+    dist = util.Counter()
+    for a in bestActions: dist[a] = bestProb / len(bestActions)
+    for a in legalActions: dist[a] += ( 1-bestProb ) / len(legalActions)
+    dist.normalize()
+    return dist
+  
 class RandomGhost( GhostAgent ):
   "A ghost that chooses a legal action uniformly at random."
   def getDistribution( self, state ):
